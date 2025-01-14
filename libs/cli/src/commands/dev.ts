0dc725adc62441b5acaa24974083bf73beb4b581
MDReal32 <veysaliyev00@gmail.com>
2025-01-14 13:06:32 +0400

    feat(cli): configure dev command env

--- libs/cli/src/commands/dev.ts
+++ libs/cli/src/commands/dev.ts
@@ -5,5 +5,11 @@ import { defineCommand } from "./index";
 export const dev = defineCommand("dev", "Starting the development environment")
   .option("port", { type: "number", defaultValue: 3000, alias: "p", description: "Port to run the server on" })
   .execute(async args => {
-    await createServer({ env: "development", server: { port: args.port } });
+    const previousEnv = process.env.NODE_ENV;
+    if (!previousEnv) {
+      process.env.NODE_ENV = "development";
+    } else if (previousEnv !== "development") {
+      throw new Error(`Cannot run development server in a other environments. Given: ${previousEnv}`);
+    }
+    await createServer({ port: args.port });
   });
