fd9647b48203486071d65494e0832be66f6e57f2
MDReal32 <veysaliyev00@gmail.com>
2025-01-15 10:02:25 +0400

    chore(resolve-url): improve regex

--- libs/client/src/utils/resolve-url.ts
+++ libs/client/src/utils/resolve-url.ts
@@ -1,9 +1,9 @@
 import { domainMapping } from "./domain-mapping";
 
-export const resolveUrl = (url: string): string => {
+export const resolveUrl = (url: string) => {
   for (const domain of domainMapping.keys()) {
     if (url.startsWith(domain)) {
-      return url.replace(domain, domainMapping.get(domain)!);
+      return url.replace(new RegExp(`^${domain}`), domainMapping.get(domain)!);
     }
   }
   if (!url.startsWith("http")) {
