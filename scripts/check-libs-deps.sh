03eb6c6c1f8393d23aeeac1d193a641f1a8134c8
MDReal32 <veysaliyev00@gmail.com>
2025-03-08 11:43:37 +0400

    feat(check-lib-deps): improvement
    Improve workspaces handling inside script

--- scripts/check-libs-deps.sh
+++ scripts/check-libs-deps.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
 LIBS_JSON="{}";
+depCheckScript=$(yarn bin depcheck)
 
 function checkDependency() {
-  local dep=$1
-  
-  # check if dep variable not empty
-  if [ -z "$dep" ]; then
-    echo "Error: dependency variable is empty"
+  local depPath=$1
+  local depName=$2
+
+  if [ -z "$depPath" ] || [ -z "$depName" ]; then
+    echo "Error: dependency path or name is empty"
     exit 1
   fi
-  
-  local depCheckScript=$(yarn bin depcheck)
 
-  pushd "libs/$dep" || exit 1
-  local json="$(node "$depCheckScript" --json)";
-  popd || exit 1
-  
-  local deps=$(echo "$json" | jq -r '.dependencies');
-  local devDeps=$(echo "$json" | jq -r '.devDependencies');
-  local missingDeps=$(echo "$json" | jq -r '.missing');
-  
-  if [[ "$deps" == "[]" && "$devDeps" == "[]" && "$missingDeps" == "{}" ]]; then
+  if [ "$depPath" == "." ]; then
     return
   fi
-  
-  # add all vars to LIBS_JSON. like { "dep": { "deps": [], "devDeps": [], "missingDeps": [] } }
-  LIBS_JSON=$(echo "$LIBS_JSON" | jq ". + { \"$dep\": { \"deps\": $deps, \"devDeps\": $devDeps, \"missingDeps\": $missingDeps } }");   
+
+  pushd "$depPath" || { echo "Error: Failed to enter $depPath"; exit 1; }
+  local json="$(node "$depCheckScript" --json)";
+  popd || exit 1
+
+  local deps=$(echo "$json" | jq -r '.dependencies // []' || echo "[]");
+  local devDeps=$(echo "$json" | jq -r '.devDependencies // []' || echo "[]");
+  local missingDeps=$(echo "$json" | jq -r '.missing // {}' || echo "{}");
+
+  if [[ "$deps" != "[]" || "$devDeps" != "[]" || "$missingDeps" != "{}" ]]; then
+    LIBS_JSON=$(echo "$LIBS_JSON" | jq ". + { \"$depName\": { \"deps\": $deps, \"devDeps\": $devDeps, \"missingDeps\": $missingDeps } }");
+  fi
 }
 
-for dir in libs/*/ ; do
-  checkDependency "$(basename "$dir")"
-done
+function processWorkspaces() {
+  WORKSPACES=$(yarn workspaces list --json | jq -c -r '{name, location}')
+
+  for row in $WORKSPACES; do
+    local name=$(echo $row | jq -r '.name')
+    local location=$(echo $row | jq -r '.location')
+    checkDependency "$location" "$name"
+  done
+}
+
+processWorkspaces
 
 echo "$LIBS_JSON" | jq --indent 2 '.' > libs-deps.json
