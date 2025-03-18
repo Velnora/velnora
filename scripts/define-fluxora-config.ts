31db79f062dc7e2c51e2eaf5d387e7c9dd510d4d
MDReal32 <veysaliyev00@gmail.com>
2025-03-18 16:46:14 +0400

    feat(fluxora-pkgs): vite configuration
    Improve vite configuration. Add .raw method for defining libs how wanna.

--- scripts/define-fluxora-config.ts
+++ scripts/define-fluxora-config.ts
@@ -1,13 +1,21 @@
-import { UserConfig, defineConfig, mergeConfig } from "vite";
+import type { InputOption } from "rollup";
+import { type UserConfig, defineConfig, mergeConfig } from "vite";
 import dtsPlugin from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export const defineFluxoraConfig = (appName: string, userConfig: UserConfig = {}) => {
+  return defineFluxoraConfig.raw(appName, { [`fluxora.${appName}`]: "src/main.ts" }, userConfig);
+};
+
+defineFluxoraConfig.raw = (appName: string, libEntry: InputOption, userConfig: UserConfig = {}) => {
   return defineConfig(
     mergeConfig<UserConfig, UserConfig>(
       {
-        mode: process.env.NODE_ENV,
-        plugins: [tsconfigPaths(), dtsPlugin({ rollupTypes: true })],
+        mode: process.env.NODE_ENV || "development",
+        plugins: [
+          tsconfigPaths(),
+          dtsPlugin({ rollupTypes: true, pathsToAliases: process.env.NODE_ENV === "development" })
+        ],
         esbuild: { target: "esnext" },
         build: {
           outDir: "build",
@@ -15,7 +23,7 @@ export const defineFluxoraConfig = (appName: string, userConfig: UserConfig = {}
           ssr: true,
           sourcemap: true,
           emptyOutDir: true,
-          lib: { entry: { [`fluxora.${appName}`]: "src/main.ts" }, name: `fluxora ${appName}`, formats: ["es"] },
+          lib: { entry: libEntry, name: `fluxora.${appName}`, formats: ["es"] },
           rollupOptions: {
             output: { chunkFileNames: "chunks/[hash].js" },
             external: [/@fluxora\/.*/]
