ebb33d324396d79fb32d7441d6947457e57ba858
MDReal32 <40066034+MDReal32@users.noreply.github.com>
2025-12-09 13:09:25 +0400

    refactor: rename core and utils files for improved structure and update entry points in configuration

--- libs/router/tsup.config.ts
+++ libs/router/tsup.config.ts
@@ -1,5 +1,5 @@
 import { defineWebConfig } from "@velnora/tooling";
 
 export default defineWebConfig({
-  entries: { "velnora.router": "src/main.ts" }
+  entries: { "velnora.router": "src/router/main.ts" }
 });
