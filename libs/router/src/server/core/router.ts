8224a188ecd8b3fe5b637da2a041829730ca8972
MDReal32 <40066034+MDReal32@users.noreply.github.com>
2025-12-24 08:03:38 +0400

    feat: extend Router constructor to accept GlobalRouter instance

--- libs/router/src/server/core/router.ts
+++ libs/router/src/server/core/router.ts
@@ -1,15 +1,18 @@
 import type { SsrRequestContext } from "@velnora/types";
 
-import { Router as ClientRouter } from "../../client/core/router";
+import { Router as ClientRouter, type GlobalRouter } from "../../client/core";
 import type { Router as VelnoraRouter } from "../../client/types/router";
 
 export class Router extends ClientRouter implements VelnoraRouter {
-  static createRouter(ctx: SsrRequestContext) {
-    return new Router(ctx);
+  static createRouter(ctx: SsrRequestContext, globalRouter: GlobalRouter) {
+    return new Router(ctx, globalRouter);
   }
 
-  protected constructor(private readonly ctx: SsrRequestContext) {
-    super(ctx.app);
+  protected constructor(
+    private readonly ctx: SsrRequestContext,
+    globalRouter: GlobalRouter
+  ) {
+    super(ctx.app, globalRouter);
   }
 
   get path() {
