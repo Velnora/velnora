a5810d0521d23f0510a86d99dbe4e339922028e4
MDReal32 <40066034+MDReal32@users.noreply.github.com>
2025-12-24 08:42:44 +0400

    feat: update module declarations to use scoped package names

--- libs/router/velnora.d.ts
+++ libs/router/velnora.d.ts
@@ -1,13 +1,13 @@
 /// <reference types="velnora/client" />
 
-declare module "velnora:applications" {
+declare module "@velnora:applications" {
   import type { Package } from "@velnora/types";
 
   export const applications: Package[];
   export const applicationsMap: Map<string, Package>;
 }
 
-declare module "velnora:bootstrap" {
+declare module "@velnora:bootstrap" {
   const _default: Record<string, string>;
   export default _default;
 }
