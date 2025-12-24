a378a5b9b3bf769980b8de4ba1a69c59ca13404e
MDReal32 <40066034+MDReal32@users.noreply.github.com>
2025-12-24 09:01:52 +0400

    fix: build issue

--- libs/generator/tsup.config.ts
+++ libs/generator/tsup.config.ts
@@ -1,5 +1,6 @@
 import { defineNodeConfig } from "@velnora/internal";
 
 export default defineNodeConfig({
-  entries: { "velnora.generator": "src/main.ts" }
+  entries: { "velnora.generator": "src/main.ts" },
+  external: ["lightningcss", "lightningcss/node"]
 });
