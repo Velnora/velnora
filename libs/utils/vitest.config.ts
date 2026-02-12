c28dbd768ea90b3936489a8fc74e41d273bdd98d
MDReal32 <40066034+MDReal32@users.noreply.github.com>
2026-02-12 20:29:20 +0400

    feat: update Vitest configuration to include both .test.ts and .spec.ts files

--- libs/utils/vitest.config.ts
+++ libs/utils/vitest.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({ test: { include: ["src/**/*.spec.ts"] } });
+export default defineConfig({ test: { include: ["src/**/*.{test,spec}.ts"] } });
