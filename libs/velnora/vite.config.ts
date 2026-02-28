cfdbe79d12dfe82d24a7ba8f20f3d1b5825bd3df
MDReal32 <40066034+MDReal32@users.noreply.github.com>
2026-02-28 13:10:04 +0400

    feat: add Vite configuration for various components and update build scripts

--- libs/velnora/vite.config.ts
+++ libs/velnora/vite.config.ts
@@ -2,5 +2,5 @@ import { defineNodeConfig } from "@velnora/internal";
 
 export default defineNodeConfig({
   name: "velnora",
-  entries: [{ input: "src/app.ts", name: "velnora.app" }]
+  entries: { "velnora.app": "src/app.ts" }
 });
